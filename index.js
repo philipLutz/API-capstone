05f2bb9b0d5bb4991fb1d2fd19a2b2ad38a6c9f8
Philip Lutz <philiplutz413@gmail.com>
2018-03-08 17:42:02 -0800

    search location

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function getDataFromWU(query) {}
+function getDataFromWU() {
+	const apiCall = 'http://api.wunderground.com/api/5e8b8ca34238674d/conditions/q/CA/San_Francisco.json'
+	$.getJSON()
+}
 
-function getDataFromIG(query) {}
+function getDataFromIG() {
+
+}
 
 function renderWeather() {}
 
@@ -10,16 +15,23 @@ function displayWeather() {}
 
 function displayPosts() {}
 
-function displaySearchLocation() {}
+function createSearchLocation(queryCity, querySC) {
+	const location = `${queryCity}, ${querySC}`;
+	$('.search-location').html(`<div>${location}</div>`);
+}
 
 function watchSubmit() {
 	$('.js-search-form').submit(function(event) {
 		event.preventDefault();
-		const queryTarget = $(event.currentTarget).find('.js-query');
-		const query = queryTarget.val();
-		queryTarget.val("");
-		getDataFromWU(query, displayWeather);
-		getDataFromIG(query, displayPosts);
+		const queryTargetCity = $(event.currentTarget).find('#js-query-city');
+		const queryCity = queryTargetCity.val();
+		queryTargetCity.val("");
+		const queryTargetSC = $(event.currentTarget).find('#js-query-state_country');
+		const querySC = queryTargetSC.val();
+		queryTargetSC.val("");
+		getDataFromWU(queryCity, querySC, displayWeather);
+		getDataFromIG(queryCity, querySC, displayPosts);
+		createSearchLocation(queryCity, querySC);
 	});
 }
 
